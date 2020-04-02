d3e80d4330b829a09adc20418f68c5c72c7d9da5
Chuck <bnsf5622@gmail.com>
2020-04-02 14:37:35 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace method_overloading_page_48
+{
+    class CallingMethod
+    {
+        public string HelloMethod()
+        {
+            return "Hello. I am from calling method";
+        }
+    }
+    class CallingMethodUsage
+    {
+        public static void Main()
+        {
+            string text = string.Empty;
+            //Object creation to activate method
+            CallingMethod obj = new CallingMethod();
+            //calling method
+            text = obj.HelloMethod();
+            Console.WriteLine(text);
+            //calling static method
+            Console.WriteLine(HelloStaticMethod());
+            Console.ReadLine();
+        }
+        public static string HelloStaticMethod()
+        {
+            return "Hello. I am from calling static method";
+        }
+    }
+}
